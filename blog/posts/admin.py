a714e821e33766b9ab4e401fb18e508b7b3eaac9
PiotrZurynski <pewdiejanusz@gmail.com>
2025-10-17 10:44:51 +0200

    Additional commit, changes in admin.py, added new model, make new migrations

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 # modele musimy zaimportować
-from .models import Category, Topic
+from .models import Category, Topic, Post
 
 # a następnie zarejestrować (pokazano najprostszy przypadek)
+
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields=["created_at"]
+    list_display=['title','text','topic','slug','created_at','updated_at','created_by']
+admin.site.register(Post)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=["name","descriptions"]
 admin.site.register(Category)
+class TopicAdmin(admin.ModelAdmin):
+    list_display=["name","category","created"]
+
 admin.site.register(Topic)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,12 +1,38 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=60)
     description=models.TextField(max_length=100,null=True,blank=True)
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering=['name']
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering=['name']
+
+class Post(models.Model):
+    title=models.CharField(max_length=150)
+    text=models.TextField()
+    topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    slug=models.SlugField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    created_by=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title #do zmiany 
+    
+    class Meta:
+        ordering=['-created_at']
